9e59d93a2e978f0861228ff90a715daf5699c406
Ryan Loos <RLoos289@gmail.com>
2016-10-20 19:42:28 -0700

    set up template and food class

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Food } from './food.model'
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <h1>Calorie Tracker</h1>
+  `
+})
+
+export class AppComponent {
+
+}
